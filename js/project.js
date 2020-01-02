5112b88076201e8cc1f41709b9f45de0cb5f22b3
Tiffany Chen <thchen@andrew.cmu.edu>
2020-01-02 14:54:06 -0800

    fixed footer

--- js/project.js
+++ js/project.js
@@ -1,11 +1,26 @@
-window.addEventListener("scroll", function() {
-    var elementTarget = document.getElementById("project-header");
+//window.addEventListener("scroll", function() {
+//    var elementTarget = document.getElementById("project-header");
+//    var scrollNav = document.getElementsByClassName("scroll-nav")[0];
+//    //    console.log(elementTarget);
+//    if (window.scrollY >= (elementTarget.offsetTop + elementTarget.offsetHeight - 30)) {
+//        console.log(scrollNav);
+//        scrollNav.style.visibility = "visible";
+//    } else{
+//        scrollNav.style.visibility = "hidden";
+//    }
+//});
+
+var menuVisibility = false;
+var pageMenu = document.getElementById("page-menu");
+pageMenu.addEventListener("click", togglePageMenu);
+
+function togglePageMenu(){
     var scrollNav = document.getElementsByClassName("scroll-nav")[0];
-    //    console.log(elementTarget);
-    if (window.scrollY >= (elementTarget.offsetTop + elementTarget.offsetHeight - 30)) {
-        console.log(scrollNav);
+    if(menuVisibility == false){
         scrollNav.style.visibility = "visible";
+        menuVisibility = true;
     } else{
         scrollNav.style.visibility = "hidden";
+        menuVisibility = false;
     }
-});
+}
