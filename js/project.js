f290fa78603723e2e4393b2f3c38429f6c920efd
Tiffany Chen <thchen@andrew.cmu.edu>
2020-01-02 16:01:05 -0800

    scrollnav show/hide working

--- js/project.js
+++ js/project.js
@@ -12,15 +12,73 @@
 
 var menuVisibility = false;
 var pageMenu = document.getElementById("page-menu");
-pageMenu.addEventListener("click", togglePageMenu);
+var menuImg = document.getElementById("menu-icon");
+var exitIcon = "../img/icons/exit.svg";
+var menuIcon = "../img/icons/menu.svg";
+pageMenu.addEventListener('click', togglePageMenu);
 
 function togglePageMenu(){
     var scrollNav = document.getElementsByClassName("scroll-nav")[0];
     if(menuVisibility == false){
-        scrollNav.style.visibility = "visible";
+        show(scrollNav);
+        menuImg.src = exitIcon;
         menuVisibility = true;
     } else{
-        scrollNav.style.visibility = "hidden";
+        hide(scrollNav);
+        menuImg.src = menuIcon;
         menuVisibility = false;
     }
 }
+
+// Show an element
+function show(elem) {
+	// Get the natural height of the element
+	var getHeight = function () {
+		elem.style.display = 'block'; // Make it visible
+		var height = elem.scrollHeight + 'px'; // Get it's height
+		elem.style.display = ''; //  Hide it again
+		return height;
+	};
+
+	var height = getHeight(); // Get the natural height
+	elem.classList.add('is-visible'); // Make the element visible
+	elem.style.height = height; // Update the max-height
+
+	// Once the transition is complete, remove the inline max-height so the content can scale responsively
+	window.setTimeout(function () {
+		elem.style.height = '';
+	}, 350);
+
+};
+
+// Hide an element
+function hide(elem) {
+
+	// Give the element a height to change from
+	elem.style.height = elem.scrollHeight + 'px';
+
+	// Set the height back to 0
+	window.setTimeout(function () {
+		elem.style.height = '0';
+	}, 1);
+
+	// When the transition is complete, hide it
+	window.setTimeout(function () {
+		elem.classList.remove('is-visible');
+	}, 350);
+
+};
+
+// Toggle element visibility
+var toggle = function (elem, timing) {
+
+	// If the element is visible, hide it
+	if (elem.classList.contains('is-visible')) {
+		hide(elem);
+		return;
+	}
+
+	// Otherwise, show it
+	show(elem);
+	
+};
